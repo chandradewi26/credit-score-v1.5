482be9ac84969ea93faf27a5a7a74232658e62b9
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-10-01 18:57:12 +1000

    Result of expected behaviour of method in test

--- Credit_Score_Xunit_Tests_v2/CalculatePointTests.cs
+++ Credit_Score_Xunit_Tests_v2/CalculatePointTests.cs
@@ -106,19 +106,19 @@ public class CalculatePointTests
     }
 
     [Theory(DisplayName = "Given customer's input, Calculate Point method returns correct Credit Point")]
-    [InlineData(100, 0, 3, 30, -1)]
-    [InlineData(1200, 1, 3, 60, -1)]
-    [InlineData(500, 1, 0, 20, -1)]
-    [InlineData(500, 1, 3, 20, -1)]
-    [InlineData(500, 5, 1, 30, -1)]
-    [InlineData(750, 1, 3, 29, -1)]
-    [InlineData(750, 2, 3, 30, -1)]
-    [InlineData(800, 0, 1, 30, -1)]
-    [InlineData(900, 0, 1, 55, -1)]
-    [InlineData(900, 1, 2, 20, -1)]
-    [InlineData(900, 2, 1, 30, -1)]
-    [InlineData(900, 3, 3, 40, -1)]
-    [InlineData(950, 0, 2, 15, -1)]
+    [InlineData(100, 0, 3, 30, 8)]
+    [InlineData(1200, 1, 3, 60, 9)]
+    [InlineData(500, 1, 0, 20, 3)]
+    [InlineData(500, 1, 3, 20, 7)]
+    [InlineData(500, 5, 1, 30, 1)]
+    [InlineData(750, 1, 3, 29, 9)]
+    [InlineData(750, 2, 3, 30, 7)]
+    [InlineData(800, 0, 1, 30, 8)]
+    [InlineData(900, 0, 1, 55, 11)]
+    [InlineData(900, 1, 2, 20, 8)]
+    [InlineData(900, 2, 1, 30, 6)]
+    [InlineData(900, 3, 3, 40, 6)]
+    [InlineData(950, 0, 2, 15, 6)]
     public void TestCalculatePoint_GivenCustomerInput(int bureauScore, int missedPaymentCount, int completedPaymentCount, int ageInYears, int expectedOutput)
     {
         var customer = new Customer(bureauScore, missedPaymentCount, completedPaymentCount, ageInYears);
