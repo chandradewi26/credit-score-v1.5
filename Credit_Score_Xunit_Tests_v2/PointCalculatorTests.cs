fcec3ce2246c45771c476b542367465f2454c29d
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-10-06 15:09:48 +1100

    Revised PointCalculatorTests filename

--- Credit_Score_Xunit_Tests_v2/PointCalculatorTests.cs
+++ Credit_Score_Xunit_Tests_v2/PointCalculatorTests.cs
@@ -1,10 +1,10 @@
 namespace Credit_Score_Xunit_Tests_v2;
 
-public class CalculatePointTests : BureauScoreCalculatorTests
+public class PointCalculatorTests
 {
     [Theory(DisplayName = "Given customer's input, Calculate Point method returns correct Credit Point")]
     [InlineData(100, 0, 3, 30, 8)]
-    [InlineData(1200, 1, 3, 60, 9)]
+    [InlineData(1200, 1, 3, 60, 12)]
     [InlineData(500, 1, 0, 20, 3)]
     [InlineData(500, 1, 3, 20, 7)]
     [InlineData(500, 5, 1, 30, 1)]
@@ -14,13 +14,13 @@ public class CalculatePointTests : BureauScoreCalculatorTests
     [InlineData(900, 0, 1, 55, 11)]
     [InlineData(900, 1, 2, 20, 8)]
     [InlineData(900, 2, 1, 30, 6)]
-    [InlineData(900, 3, 3, 40, 6)]
-    [InlineData(950, 0, 2, 15, 6)]
+    [InlineData(900, 3, 3, 40, 6)]   
+    [InlineData(950, 0, 2, 15, 6)] 
     public void TestCalculatePoint_GivenCustomerInput(int bureauScore, int missedPaymentCount, int completedPaymentCount, int ageInYears, int expectedOutput)
     {
         var customer = new Customer(bureauScore, missedPaymentCount, completedPaymentCount, ageInYears);
-        var calculator = new CreditCalculator();
-        var result = calculator.CalculatePoint(customer);
+        var calculator = new PointCalculator();
+        var result = calculator.Calculate(customer);
         Assert.Equal(expectedOutput, result);
     }
 }
